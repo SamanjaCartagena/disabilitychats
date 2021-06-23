277cf0346a5b8aa403fba46e40ec46f69dd00399
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-06-22 21:43:15 -0400

    tailwind failed

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,71 +1,113 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
+import { useFirestoreQuery } from '../hooks';
+// Components
 import Message from './Message';
 
-const Channel = ({user=null, db= null})=>{
-    const [messages,setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState('');
+const Channel = ({ user = null }) => {
+  const db = firebase.firestore();
+  const messagesRef = db.collection('messages');
+  const messages = useFirestoreQuery(
+    messagesRef.orderBy('createdAt', 'desc').limit(100)
+  );
 
-    const {uid, displayName, photoURL} = user;
+  const [newMessage, setNewMessage] = useState('');
 
-    useEffect(() =>{
-        if(db){
-            const unsubscribe= db
-            .collection('messages')
-            .orderBy('messagedAt')
-            .limit(100)
-            .onSnapshot(querySnapshot =>{
+  const inputRef = useRef();
+  const bottomListRef = useRef();
 
-                const data = querySnapshot.docs.map(doc => ({
-                    ...doc.data(),
-                    id:doc.id,
-                }));
-                setMessages(data);
+  const { uid, displayName, photoURL } = user;
 
-            })
-
-            return unsubscribe;
-        }
-
-    },[db]);
-const handleOnChange = e =>{
-    setNewMessage(e.target.value);
-
-};
-
-const handleOnSubmit = e =>{
-    e.preventDefault();
-    if(db){
-        db.collection('messages').add({
-            text:newMessage,
-            messagedAt: firebase.firestore.FieldValue.serverTimestamp(),
-            uid,
-            displayName,
-            photoURL
-        })
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
     }
-}
+  }, [inputRef]);
 
-    return (
-        <>
-        <ul>
-            {messages.map(message =>(
-                <li key={message.id}><Message {...message}/></li>
-            ))}
-        </ul>
-       <form onSubmit={handleOnSubmit}>
-           <input
-                 type="text"
-                 value={newMessage}
-                 onChange={handleOnChange}
-                 placeholder="Type your message here ....l"
+  const handleOnChange = e => {
+    setNewMessage(e.target.value);
+  };
 
-/>
-<button type="submit" disabled={!newMessage}>Submit</button>
-       </form>
+  const handleOnSubmit = e => {
+    e.preventDefault();
 
-        </>
-       
-    );
+    const trimmedMessage = newMessage.trim();
+    if (trimmedMessage) {
+      // Add new message in Firestore
+      messagesRef.add({
+        text: trimmedMessage,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        displayName,
+        photoURL,
+      });
+      // Clear input field
+      setNewMessage('');
+      // Scroll down to the bottom of the list
+      bottomListRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="overflow-auto h-full">
+        <div className="py-4 max-w-screen-lg mx-auto">
+          <div className="border-b dark:border-gray-600 border-gray-200 py-8 mb-4">
+            <div className="font-bold text-3xl text-center">
+              <p className="mb-1">Welcome to</p>
+              <p className="mb-3">React FireChat</p>
+            </div>
+            <p className="text-gray-400 text-center">
+              This is the beginning of this chat.
+            </p>
+          </div>
+          <ul>
+            {messages
+              ?.sort((first, second) =>
+                first?.createdAt?.seconds <= second?.createdAt?.seconds ? -1 : 1
+              )
+              ?.map(message => (
+                <li key={message.id}>
+                  <Message {...message} />
+                </li>
+              ))}
+          </ul>
+          <div ref={bottomListRef} />
+        </div>
+      </div>
+      <div className="mb-6 mx-4">
+        <form
+          onSubmit={handleOnSubmit}
+          className="flex flex-row bg-gray-200 dark:bg-coolDark-400 rounded-md px-4 py-3 z-10 max-w-screen-lg mx-auto dark:text-white shadow-md"
+        >
+          <input
+            ref={inputRef}
+            type="text"
+            value={newMessage}
+            onChange={handleOnChange}
+            placeholder="Type your message here..."
+            className="flex-1 bg-transparent outline-none"
+          />
+          <button
+            type="submit"
+            disabled={!newMessage}
+            className="uppercase font-semibold text-sm tracking-wider text-gray-500 hover:text-gray-900 dark:hover:text-white transition-colors"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
+
+Channel.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string,
+    displayName: PropTypes.string,
+    photoURL: PropTypes.string,
+  }),
+};
+
 export default Channel;
